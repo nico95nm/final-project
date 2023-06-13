b92337e5617bb3451e01b5a838d8c563dc1479d4
LazyMan95 <nico95.nm@gmail.com>
2023-06-13 18:08:45 +0200

    ADD final-project-0.0.1v

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
 import Image from 'next/image';
-import styles from './page.module.scss';
+import styles from './page.global.scss';
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <div>
+        <div>
+          <span>Annoucement</span> <br />
+          <span>General Chat</span> <br />
+          <span>Gaming News</span> <br />
+          <span>Gaming Topic</span> <br />
+          <span>Gaming Music</span> <br />
+          <span>Annoucement</span> <br />
+          <span>Annoucement</span>
+        </div>
+      </div>
+    </main>
+  );
 }
