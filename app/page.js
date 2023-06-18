0023c21b5cab7e6461bcf5974b269dc1501a9e86
-replace-all <nico95.nm@gmail.com>
2023-06-18 18:20:36 +0200

    ADD Final changes to HTML(for now

--- app/page.js
+++ app/page.js
@@ -8,28 +8,21 @@ export default function Home() {
         <div className="mx-56 flex flex-col justify-center  text-blue-600 ">
           <div>
             <h2 className=" font-inter box-border h-10  flex items-center   w-50 p-4 border-1 px-3 py-4 b bg-[#0d202d] text-white">
-              GAMING FORUM - VIDEO GAME FORUMS
+              IMPORTANT
             </h2>
           </div>
-          <ul>
-            <li
-              className="pl-10 flex
-                          items-start justify-around	"
-            >
-              <span className="">icon</span>
-              <div className="">
+          <ul className=" p-2 flexbox justify-center items-center h-20 ">
+            <li className="pl-10 flex items-start justify-around ">
+              <span>icon</span>
+              <div>
                 <Link href="/topics/announcements">Announcements</Link>
                 <p className="text-black">
                   This is a page for a this sites announcements.
                 </p>
               </div>
-              <div className="p-5">
-                <dt>80</dt>
-              </div>
-              <div>
-                <span className="px-8">img</span>
-                <span>username</span>
-              </div>
+              <dt>80</dt>
+              <span>img</span>
+              <span>username</span>
             </li>
           </ul>
           <div>
@@ -37,85 +30,70 @@ export default function Home() {
               VIDEO GAME DISCUSSIONS
             </h2>
             <nav>
-              <ul>
-                <li
-                  className="pl-10 flex
-                          items-start justify-around	"
-                >
-                  <span className="">icon</span>
-                  <div className="p-8">
+              <ul className="pt-6 p-2 flexbox justify-center items-center ">
+                <li className="pl-10 flex items-start justify-around	">
+                  <span>icon</span>
+                  <div>
                     <Link href="/topics/generalChat">General Games Chat</Link>
-                    <p className="text-black">
+                    <p className="text-black trudance">
                       This is the forum to post any general topics relating to
-                      gaming at large. Chat about your favorite games, recent
-                      purchases and all the platforms.
+                      gaming at large.
+                      <br /> Chat about your favorite games, recent purchases
+                      and all the platforms.
                     </p>
                   </div>
-                  <div className="p-8">
-                    <dt className="p-8">80</dt>
-                  </div>
-                  <div>
-                    <span className="p-8 ">img</span>
-                    <span className="">username</span>
-                  </div>
+                  <dt>80</dt>
+                  <span>img</span>
+                  <span>username</span>
                 </li>
-                <li
-                  className="pl-10 flex
-                          items-start justify-around	"
-                >
-                  <span className="p-8">icon</span>
-                  <div className="p-8">
+
+                <li className="pl-10 flex items-start justify-around py-4">
+                  <span>icon</span>
+                  <div>
                     <Link href="/topics/gamingNews">Gaming News</Link>
                     <p className="text-black">
-                      Catch up on the latest buzz in the gaming world. Here you
-                      can discuss news and rumors on games, developers and more.
+                      Catch up on the latest buzz in the gaming world.
+                      <br /> Here you can discuss news and rumors on games,
+                      developers and more.
                     </p>
                   </div>
-                  <div className="p-8">
+                  <dt>80</dt>
+                  <span>img</span>
+                  <span>username</span>
+                </li>
+
+                <li>
+                  <div className="pl-10 flex items-start justify-around py-4">
+                    <span>icon</span>
+                    <div>
+                      <Link href="/topics/gamingTopic">Gaming Topic</Link>
+                      <p className="text-black">
+                        This is the forum to post any general topics relating to
+                        gaming at large.
+                        <br /> Chat about your favorite games, recent purchases,
+                        etc.
+                      </p>
+                    </div>
                     <dt>80</dt>
-                  </div>
-                  <div>
-                    <span className="p-8">img</span>
+                    <span>img</span>
                     <span>username</span>
                   </div>
                 </li>
-                <li
-                  className="pl-10 flex
-                          items-start justify-around"
-                >
-                  <span className="p-8">icon</span>
-                  <div className="p-8">
-                    <Link href="/topics/gamingTopic">Gaming Topic</Link>
-                    <p className="text-black">
-                      This is the forum to post any general topics relating to
-                      gaming at large. Chat about your favorite games, recent
-                      purchases, etc.
-                    </p>
-                  </div>
-                  <div className="p-8">
-                    <dt>80</dt>
-                  </div>
+
+                <li className="pl-10 flex items-start	justify-around py-4">
+                  <span>icon</span>
                   <div>
-                    <span className="p-8">img</span>
-                    <span>username</span>
-                  </div>
-                </li>
-                <li
-                  className="pl-10 flex
-                          items-start	justify-around	"
-                >
-                  <span className="p-8">icon</span>
-                  <div className="p-8">
                     <Link href="/topics/gamingMusic">Gaming Music</Link>
-                    <p className="text-black">This is a paragraph</p>
+                    <p className="text-black">
+                      Gaming forum buzzing with discussions on captivating video
+                      game soundtracks,
+                      <br /> where users share favorites, recommend hidden gems,
+                      and praise talented composers.
+                    </p>
                   </div>
-                  <div className="p-8">
-                    <dt>80</dt>
-                  </div>
-                  <div>
-                    <span className="p-8">img</span>
-                    <span>username</span>
-                  </div>
+                  <dt>80</dt>
+                  <span>img</span>
+                  <span>username</span>
                 </li>
               </ul>
             </nav>
@@ -125,39 +103,31 @@ export default function Home() {
               COMMUNITY
             </h2>
             <ul>
-              <li
-                className="pl-10 flex
-                          items-start justify-around	"
-              >
-                <span className="p-8">icon</span>
-                <div className="p-8">
+              <li className="pl-10 flex items-start justify-around py-8">
+                <span>icon</span>
+                <div>
                   <Link href="/topics/offTopic">Off Topics</Link>
                   <p className="text-black">This is a paragraph</p>
                 </div>
-                <div>
-                  <dt>80</dt>
-                </div>
-                <div>
-                  <span className="p-8">img</span>
-                  <span>username</span>
-                </div>
+
+                <dt>80</dt>
+                <span>img</span>
+                <span>username</span>
               </li>
               <li
                 className="pl-10 flex
-                          items-start justify-around"
+                          items-start justify-around py-8"
               >
-                <span className="p-8">icon</span>
-                <div className="p-8">
+                <span>icon</span>
+                <div>
                   <Link href="/generalChat">Intorduction</Link>
                   <p className="text-black">This is a pasddsdagraph</p>
                 </div>
                 <div>
                   <dt>80</dt>
                 </div>
-                <div>
-                  <span className="p-8">img</span>
-                  <span>username</span>
-                </div>
+                <span>img</span>
+                <span>username</span>
               </li>
             </ul>
           </div>
